5353e585dbdbd19fa5fc35a9b44faa8bc8c0ad4f
bai <1158753601@qq.com>
2020-01-03 10:51:10 +0800

    1

---
+++ src/com/edu/git/MyGitTest2.java
@@ -0,0 +1,10 @@
+package com.edu.git;
+
+public class MyGitTest2 {
+    public static void main(String[] args) {
+        System.out.println("XXXXXXXX");
+        System.out.println("YYYYYYYY");
+
+        System.out.println("1111");
+    }
+}
