bdd7f207abd37d4eab38927294e3597d5457f230
bai <1158753601@qq.com>
2020-01-03 10:25:13 +0800

    白 第三次测试

--- src/com/edu/git/MyGitTest.java
+++ src/com/edu/git/MyGitTest.java
@@ -3,5 +3,7 @@ package com.edu.git;
 public class MyGitTest {
     public static void main(String[] args) {
         System.out.println("XXXXXXXX");
+        System.out.println("YYYYYYYY");
+        //todo
     }
 }
